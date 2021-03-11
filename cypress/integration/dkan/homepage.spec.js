6459692a68769acdbd0f3d60467df39e68eef208
dgading <dgading@gmail.com>
2021-03-11 15:25:31 -0500

    Add topics and tests

--- cypress/integration/dkan/homepage.spec.js
+++ cypress/integration/dkan/homepage.spec.js
@@ -1,14 +1,32 @@
 describe('DKAN Homepage', () => {
-  it('Loads a site', () => {
+  beforeEach(() => {
     cy.visit('/');
+    cy.fixture('home/topics').as('topicJson');
+  })
+
+  it.skip('Loads a site', () => {
     cy.get('.dc-site-name a').should('have.text', 'Open Data Catalog');
   });
   it('Test with Testing Library', () => {
-    cy.visit('/');
     // Failing Test
     // cy.findByRole('link', {name: 'Open Data Catalog'}).should('exist');
     cy.findByAltText('Open Data Catalog').should('exist');
     cy.findAllByRole('link', {name: 'Open Data Catalog'}).its('length').should('eq', 2);
     cy.get('.dc-site-name').findByRole('link', {name: 'Open Data Catalog'}).should('exist');
+  });
+  it.only('Has a topics sections', () => {
+    cy.get('@topicJson').then((json) => {
+      cy.findByRole('heading', { name: json.title, level: 2}).should('exist');
+      json.topics.forEach((topic) => {
+        // Fails
+        // cy.findByRole('link', { name: topic.title, exact: false }).should('have.attr', 'href', topic.url)
+        cy.findByText(topic.title).should('exist');
+      })
+      const links = cy.get('.dc-icon-list').findAllByRole('link');
+      links.each((link) => {
+        const topicIndex = json.topics.findIndex((topic) => topic.url === link[0].href);
+        expect(topicIndex).to.be.greaterThan(-1);
+      });
+    })
   });
 });
