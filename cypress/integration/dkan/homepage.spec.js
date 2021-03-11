8b5e290cadba1f719611f51322453041ab24f482
dgading <dgading@gmail.com>
2021-03-11 14:02:29 -0500

    Add testing library and homepage test

--- cypress/integration/dkan/homepage.spec.js
+++ cypress/integration/dkan/homepage.spec.js
@@ -2,5 +2,13 @@ describe('DKAN Homepage', () => {
   it('Loads a site', () => {
     cy.visit('/');
     cy.get('.dc-site-name a').should('have.text', 'Open Data Catalog');
-  })
+  });
+  it('Test with Testing Library', () => {
+    cy.visit('/');
+    // Failing Test
+    // cy.findByRole('link', {name: 'Open Data Catalog'}).should('exist');
+    cy.findByAltText('Open Data Catalog').should('exist');
+    cy.findAllByRole('link', {name: 'Open Data Catalog'}).its('length').should('eq', 2);
+    cy.get('.dc-site-name').findByRole('link', {name: 'Open Data Catalog'}).should('exist');
+  });
 });
